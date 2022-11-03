588e7d131dc3a5ba389735129f69710d139b4b24
aipoh ruth <aipohruth@gmail.com>
2022-11-03 20:00:28 +0100

    style:updated media query

--- src/contact.js
+++ src/contact.js
@@ -3,19 +3,22 @@ import React from 'react'
 const Contact = () => {
   return (
     <section className='contact'>
+      
         <div className="form-header">
         <h1>Contact Me</h1>
         <p>Hi there, contact me to ask me about anything you have in mind</p>
         </div>
         <form className='form'>
-        <div className="form-control">
-            <label htmlFor='firstname'>First Name
-            <input type='text' id='first_name' placeholder='Enter your first name' />
-            </label>
-             <label className='second' htmlFor='lastname'>Last Name
-            <input type='text' id='last_name' placeholder='Enter your last name' />
-            </label>
-        </div>
+          <div className="form-controls">
+            <div className="left">
+              <label>Firstname</label>
+              <input type='text' id='first_name'placeholder='Enter your first name'/>
+            </div>
+            <div className="right">
+              <label>Lastname</label>
+              <input type='text' id='last_name' placeholder='Enter your last name' />
+            </div>
+          </div>
 
         <div className="form-control">
             <label>Email</label>
@@ -30,7 +33,9 @@ const Contact = () => {
         <small>You agree to providing your data to aipohruth who may contact you</small>
         </div>
 
+          <div className="button">
         <button type='submit' id='btn__submit'>Send message</button>
+        </div>
       
     </form>
 
@@ -42,3 +47,12 @@ const Contact = () => {
 }
 
 export default Contact
+
+/* <div className="form-control">
+            <label htmlFor='firstname'>First Name
+            <input type='text' id='first_name' placeholder='Enter your first name' />
+            </label>
+             <label className='second' htmlFor='lastname'>Last Name
+            <input type='text' id='last_name' placeholder='Enter your last name' />
+            </label>
+        </div> */
