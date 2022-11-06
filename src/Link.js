069c50bb90a8e91435c2b216cdcaa66c4525cff6
aipoh ruth <aipohruth@gmail.com>
2022-11-06 12:56:12 +0100

    fix: fix a bug

--- src/Link.js
+++ src/Link.js
@@ -17,13 +17,13 @@ const Link = () => {
     <h3 id='twitter'>Madame Lisa</h3>
     <p id='slack'>aipohRuth</p>
     <div className='links'>
-        <button id='twitter__link'><a href='https://twitter.com/M_e_l_y_sa'>Twitter Link</a></button>
-        <button id='btn__zuri'><a href="https://training.zuri.team/">Zuri Team</a></button>
-        <button id='books'><a href="http://books.zuri.team">Zuri Books</a></button>
-        <button id='book__python'><a href="https://books.zuri.team/python-for-beginners?ref_id=aipohRuth">Python Books</a></button>
-        <button id='pitch'><a href="https://background.zuri.team">Background check for coders</a></button>
-        <button id='book__design'><a href="https://books.zuri.team/design-rules">Design Books</a></button>
-        <button id='contact'><a href="/contact">Contact Me</a></button>
+       <a id='twitter__link' href='https://twitter.com/M_e_l_y_sa'>Twitter Link</a>
+        <a id='btn__zuri' href="https://training.zuri.team/">Zuri Team</a>
+        <a  id='books' href="http://books.zuri.team">Zuri Books</a>
+        <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=aipohRuth">Python Books</a>
+        <a   id='pitch'href="https://background.zuri.team">Background check for coders</a>
+        <a  id='book__design'href="https://books.zuri.team/design-rules">Design Books</a>
+        <a id='contact' href="/contact">Contact Me</a>
     </div>
     <div className="icons">
       <img src={f1}  alt='slack' />
